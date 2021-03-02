aaa509b2ead72e55d134b152310ef5a8deaabe01
SEONKYU\difke <somedaycode@gmail.com>
2021-03-02 17:32:53 +0900

    view ejs 엔진을 사용하지 않고 html로 렌더

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -44,7 +45,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error.html');
 });
 
 module.exports = app;
