5be1d9a8aca14085c2e237bfd38d2ca9254bb8d3
SEONKYU\difke <somedaycode@gmail.com>
2021-03-03 19:02:30 +0900

    calculate.js에 strToHex 함수 추가

--- public/js/calculate.js
+++ public/js/calculate.js
@@ -11,10 +11,14 @@ function getArcDeg({ degree, arcNum }) {
   return degree / arcNum;
 }
 
+function strToHex(str) {
+  return str.split('').map((str) => str.charCodeAt(0).toString(16));
+}
+
 function getTextCoordinates(textDeg, { radius, width, height }) {
   const x = Math.cos(degToRadians(textDeg)) * radius * 0.8 + width / 2;
   const y = Math.sin(degToRadians(textDeg)) * radius * 0.8 + height / 2;
   return [x, y];
 }
 
-export { degToRadians, hexToDec, getArcDeg, getTextCoordinates };
+export { degToRadians, hexToDec, getArcDeg, strToHex, getTextCoordinates };
