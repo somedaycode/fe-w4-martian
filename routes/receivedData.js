1dfe8c14d1ecc44956987ae70e9675d28b942656
SEONKYU\difke <somedaycode@gmail.com>
2021-03-03 17:58:59 +0900

    메시지를 json 파일 형태로 바꾸고 data 폴더에 저장하는 라우터 기능 추가
    클라이언트에서 메시지를 입력하고 버튼을 누르면 값이 json 형태로 저장된다.

--- routes/receivedData.js
+++ routes/receivedData.js
@@ -8,6 +8,10 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
+<<<<<<< HEAD
+=======
+  res.send('정상적으로 메시지를 보냈습니다.');
+>>>>>>> d758776 (메시지를 json 파일 형태로 바꾸고 data 폴더에 저장하는 라우터 기능 추가)
   const data = JSON.stringify(req.body);
   fs.writeFile(
     '../FE-W4-MARTIAN/public/data/receivedData.json',
@@ -17,8 +21,11 @@ router.post('/', function (req, res, next) {
       if (err) alert('에러입니다: receovedData.js 파일 참조');
     }
   );
+<<<<<<< HEAD
 
   res.send('정상적으로 메시지를 보냈습니다.');
+=======
+>>>>>>> d758776 (메시지를 json 파일 형태로 바꾸고 data 폴더에 저장하는 라우터 기능 추가)
 });
 
 module.exports = router;
