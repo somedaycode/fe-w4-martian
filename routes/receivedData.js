d44d4948deb429dce147ffd1813e58b2c2a02194
SEONKYU\difke <somedaycode@gmail.com>
2021-03-03 19:20:17 +0900

    routes 정상적으로 데이터를 받았을 떄 출력하는 메시지 수정

--- routes/receivedData.js
+++ routes/receivedData.js
@@ -8,7 +8,6 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-  res.send('정상적으로 메시지를 보냈습니다.');
   const data = JSON.stringify(req.body);
   fs.writeFile(
     '../FE-W4-MARTIAN/public/data/receivedData.json',
@@ -18,6 +17,8 @@ router.post('/', function (req, res, next) {
       if (err) alert('에러입니다: receovedData.js 파일 참조');
     }
   );
+
+  res.send('정상적으로 메시지를 보냈습니다.');
 });
 
 module.exports = router;
