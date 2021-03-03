1b70ce5d92d64138ef74ee64fdf607090574b234
SEONKYU\difke <somedaycode@gmail.com>
2021-03-03 17:55:28 +0900

    지구에서 보내는 메시지 함수 executeTransmitter추가

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 import { _ } from './util.js';
 import { drawTransceiver } from './drawTransceiver.js';
+import { executeTransmitter } from './transmitter.js';
 
 const main = () => {
   drawTransceiver();
+  executeTransmitter();
 };
 
 main();
