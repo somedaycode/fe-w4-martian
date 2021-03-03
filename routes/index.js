0e0540776186ec819674779ea03fa763aed793d9
SEONKYU\difke <somedaycode@gmail.com>
2021-03-03 11:09:40 +0900

    -

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+<<<<<<< HEAD
   res.render('index.html');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+  res.render('index.html');
+=======
+  res.render('index');
+>>>>>>> 06a80f4 (개발환경 구성 커밋)
+=======
+  res.render('index.html');
+>>>>>>> aaa509b (view ejs 엔진을 사용하지 않고 html로 렌더)
+>>>>>>> ac7d1f8 (-)
 });
 
 module.exports = router;
