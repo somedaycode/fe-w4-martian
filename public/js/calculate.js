2b93d28ee367d0d464a91f88a9a7c18f05482a2c
SEONKYU\difke <somedaycode@gmail.com>
2021-03-03 13:28:55 +0900

    계산하여 return하는 함수들은 모두 calculate.js로 이동함
    각도를 레디안으로 변경,
    10진수를 16진수로 변환,
    부채꼴의 각도를 구함,
    부채꼴 안에 들어가는 text의 좌표를 구함

--- public/js/calculate.js
+++ public/js/calculate.js
@@ -11,14 +11,21 @@ function getArcDeg({ degree, arcNum }) {
   return degree / arcNum;
 }
 
+<<<<<<< HEAD
 function strToHex(str) {
   return str.split('').map((str) => str.charCodeAt(0).toString(16));
 }
 
+=======
+>>>>>>> c556e79 (계산하여 return하는 함수들은 모두 calculate.js로 이동함)
 function getTextCoordinates(textDeg, { radius, width, height }) {
   const x = Math.cos(degToRadians(textDeg)) * radius * 0.8 + width / 2;
   const y = Math.sin(degToRadians(textDeg)) * radius * 0.8 + height / 2;
   return [x, y];
 }
 
+<<<<<<< HEAD
 export { degToRadians, hexToDec, getArcDeg, strToHex, getTextCoordinates };
+=======
+export { degToRadians, hexToDec, getArcDeg, getTextCoordinates };
+>>>>>>> c556e79 (계산하여 return하는 함수들은 모두 calculate.js로 이동함)
