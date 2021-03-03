0bf4cba513b6314a63770c51823a461c7c25896b
SEONKYU\difke <somedaycode@gmail.com>
2021-03-03 16:27:31 +0900

    json data 생성을 위한 폴더 추가

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,10 @@
 const express = require('express');
-<<<<<<< HEAD
-=======
 const fs = require('fs');
->>>>>>> 553c597 (지구로 보내는 메시지를 라우터를 통해 응답받고 json 형태로 데이터 폴더에 생성)
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-<<<<<<< HEAD
-<<<<<<< HEAD
   res.render('index.html');
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-  res.render('index.html');
-=======
-  res.render('index');
->>>>>>> 06a80f4 (개발환경 구성 커밋)
-=======
-  res.render('index.html');
->>>>>>> aaa509b (view ejs 엔진을 사용하지 않고 html로 렌더)
->>>>>>> ac7d1f8 (-)
-=======
-  res.render('index.html');
->>>>>>> e26609e (git rebase 충돌로 인한 파일 업데이트)
 });
 
 router.post('/receivedData', function (req, res, next) {
