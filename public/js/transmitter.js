382a9457293125ddb9d7054f100a0b1ae52d73db
SEONKYU\difke <somedaycode@gmail.com>
2021-03-04 18:52:27 +0900

    delete 함수 모듈화로 인한 transmiiter.js 내 화살표 동작 함수들 삭제

--- public/js/transmitter.js
+++ public/js/transmitter.js
@@ -1,12 +1,8 @@
 import { _ } from './util.js';
-import { hexToDec, strToHex } from './calculate.js';
-import {
-  canvas,
-  ctx,
-  getFigure,
-  insertText,
-  strokeFigure,
-} from './drawTransceiver.js';
+import { strToHex } from './calculate.js';
+import { moveTransceiverArrow } from './operateTransceiver.js';
+
+// operateTranceiver.js의 함수를 통해 화살표가 움직이게 됩니다.
 
 export function executeTransmitter() {
   onInputMessages();
@@ -56,42 +52,4 @@ function enCodeText({ target }) {
 // 회전판 동작이 마무리 되면 사용한다. 현재 주석처리 해놓았다.
 function initInputValue(...inputs) {
   return inputs.forEach((input) => (input.value = ''));
-}
-
-function moveTransceiverArrow([...enCodedStrs]) {
-  const delay = 3000;
-  enCodedStrs.forEach((str, i) => {
-    setTimeout(() => rotateArrow(str, i), delay * (i + 1));
-  });
-}
-
-function rotateArrow(str) {
-  if (str === ',') return console.log('다음 단어를 기다리고 있습니다.');
-  const arrow = _.$('.arrow');
-  // 더 가까운 쪽으로 가기 위해 currentDeg 값을 구해준다. - 아직 미구현
-  const currentDeg = arrow.style.transform.match(/\d{0,3}\.\d{0,2}/g);
-  const oneArcDeg = 22.5;
-  const defalutDeg = oneArcDeg / 2;
-  const decNumber = hexToDec(str);
-  const movingDeg = defalutDeg + oneArcDeg * decNumber;
-  arrow.style.transform = `rotate(${movingDeg}deg)`;
-  paintPointedStr(decNumber, oneArcDeg);
-}
-
-function paintPointedStr(decNumber, oneArcDeg) {
-  const figure = getFigure(canvas);
-  figure.degree = oneArcDeg * decNumber;
-  figure.arcDegree = oneArcDeg;
-  ctx.beginPath();
-  ctx.moveTo(figure.width / 2, figure.height / 2);
-  ctx.lineWidth = '2';
-  ctx.strokeStyle = '#f39c12';
-  insertText(figure, decNumber);
-  initTransceiver(figure);
-}
-
-function initTransceiver(figure) {
-  setTimeout(() => {
-    strokeFigure(figure);
-  }, 1500);
 }
