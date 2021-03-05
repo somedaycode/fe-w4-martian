104cf2210754b63e88256b5aba5318798b41e475
SEONKYU\difke <somedaycode@gmail.com>
2021-03-05 21:01:08 +0900

    add excuteReceiver 함수 추가 - 수신정보를 관리한다.

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,12 @@
 import { _ } from './util.js';
 import { drawTransceiver } from './drawTransceiver.js';
 import { executeTransmitter } from './transmitter.js';
+import { executeReceiver } from './receiver.js';
 
 const main = () => {
   drawTransceiver();
   executeTransmitter();
+  executeReceiver();
 };
 
 main();
